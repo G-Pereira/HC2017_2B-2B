d004f4852fab22b10445ac32fdfbdc00884114f1
Daniel Silva <daniel_p_silva_97@hotmail.com>
2017-02-23 19:21:57 +0000

    Endpoint Missing

--- HC17/HC17/Endpoint.cs
+++ HC17/HC17/Endpoint.cs
@@ -8,5 +8,9 @@ namespace HC17
 {
 	class Endpoint
 	{
+		public uint id { get; set; }
+		public uint nCacheServers { get; set; }
+		public uint dc_latency { get; set; }
+		public CacheServer[] cache_servers { get; set; } 
 	}
 }
