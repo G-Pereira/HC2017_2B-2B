90feedaec15f2b200e5a43614d586dc44d946384
Daniel Silva <daniel_p_silva_97@hotmail.com>
2017-02-23 18:39:03 +0000

    Read the first line!

--- HC17/HC17/Program.cs
+++ HC17/HC17/Program.cs
@@ -10,22 +10,25 @@ namespace ConsoleApplication4
     {
         static void Main(string[] args)
         {
-            int counter = 0;
-            string line;
+			// Open the file.
+			System.IO.StreamReader file = new System.IO.StreamReader("c:\\kittens.in");
 
+			string line;
+			string[] lines;
             uint nVideos;
-            uint endpoints;
+            uint nEndpoints;
+            uint nRequests;
+            uint nCacheServers;
+            uint cacheServersCapacity;
 
-            // Read the file and display it line by line.
-            System.IO.StreamReader file = new System.IO.StreamReader("c:\\kittens.in");
-            while ((line = file.ReadLine()) != null)
-            {
-                //Console.WriteLine(line);
-                counter++;
-            }
-
-            file.Close();
-            Console.WriteLine(counter);
+            if(((line = file.ReadLine()) != null)) {
+				lines=line.Split(' ');
+				nVideos					= Convert.ToUInt32(lines[0]);
+				nEndpoints				= Convert.ToUInt32(lines[1]);
+				nRequests				= Convert.ToUInt32(lines[2]);
+				nCacheServers			= Convert.ToUInt32(lines[3]);
+				cacheServersCapacity	= Convert.ToUInt32(lines[4]);
+			}
 
             // Suspend the screen.
             Console.ReadLine();
